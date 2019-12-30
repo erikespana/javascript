f99058d1a0c0164a7929b0f2b908513d90b7e409
Erik Espana <erikespana@users.noreply.github.com>
2019-12-30 08:16:13 -0600

    Created named function

--- asynchronous/callbacks.js
+++ asynchronous/callbacks.js
@@ -1,18 +1,27 @@
+// Array mimicks blog posts on a server.
 const posts = [
   { title: 'Post One', body: 'This is post one' },
   { title: 'Post Two', body: 'This is post two' }
 ];
 
+
+// Gets blog posts
 function getPosts() {
-  setTimeout(() => {
-    let output = '';
-    posts.forEach((post, index) => {
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
+  // Use set timeout to mimick fetching blog posts from server.
+  // Parameters: Callout arrow function, 1 second delay.
+  setTimeout( changeInnerHTML , 1000);
 }
 
+let changeInnerHTML = function () {
+  let output = '';
+  posts.forEach((post, index) => {
+    // Append
+    output += `<li>${post.title}</li>`;
+  });
+  document.body.innerHTML = output;
+}
+
+// Creating blog posts
 function createPost(post, callback) {
   setTimeout(() => {
     posts.push(post);
